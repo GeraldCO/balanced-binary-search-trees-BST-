93b3cb31c8424e9bc90d872f45ad3056bb4e6d48
Gerald <geraldcerd@gmail.com>
2026-01-19 09:02:06 -0600

    01/19

--- Tree.js
+++ Tree.js
@@ -24,16 +24,5 @@ class Tree{
 
     }
 
-    prettyPrint(node, prefix = '', isLeft = true){
-        if(node === null){
-            return;
-        }
-        if(node.right !== null){
-            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-        }
-        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-        if(node.left !== null){
-            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-        }
-    }
+    
 }
