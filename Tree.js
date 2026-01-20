95dccb827f15d9ec13f270367ee1cbaeab592d52
Gerald <geraldcerd@gmail.com>
2026-01-19 23:31:28 -0600

    01/19 - 2

--- Tree.js
+++ Tree.js
@@ -24,5 +24,30 @@ class Tree{
 
     }
 
+    insert(value, tree){
+        let tempTree;
+        if(tree.data == value){
+            return null;
+        }
+        if(tree.data > value){
+            if(tree.left === null){
+                tree.left = new Node(value);
+            }else{
+                this.insert(value, tree.left);
+            }
+        }
+        if(tree.data < value){
+            if(tree.Right == null){
+                tree.right.data = value;
+            }else{
+                this.insert(value, tree.right);
+            }            
+        }
+    }
+
+    deleteItem(value){
+
+    }
+
     
 }
